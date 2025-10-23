45742c5ba773b17a1b088131dac83354820fa045
leroquan <anne.leroquais@eawag.ch>
2025-10-23 10:10:35 +0200

    Added Rho in the kinetic energy computation

--- 03-Run/functions/lvl0_functions.py
+++ 03-Run/functions/lvl0_functions.py
@@ -1,5 +1,5 @@
-def compute_ke_snapshot(uvel, vvel, wvel, dx, dy, dz):
-    ke = 0.5 * (uvel ** 2 + vvel ** 2 + wvel ** 2) * dx * dy * dz  # This gives J per cell
+def compute_ke_snapshot(uvel, vvel, wvel, dx, dy, dz, rho=1000.0):
+    ke = 0.5 * rho * (uvel ** 2 + vvel ** 2 + wvel ** 2) * dx * dy * dz  # This gives J per cell
 
     return ke / 1e6  # Convert to MJ
 
